71d44c4cc91193146f70409772cb7808d5297968
Gwenn <gwenn.garrigues@gmail.com>
2025-11-29 21:38:58 +0100

    major changement : renaming folder and creating new folder - adding new methods

--- collecte.py
+++ collecte.py
@@ -5,6 +5,9 @@ from qiskit_ibm_runtime import QiskitRuntimeService
 import datetime
 import pandas as pd
 import numpy as np
+import json
+
+import os 
 
 def connect():
     QiskitRuntimeService.save_account(
@@ -27,44 +30,121 @@ def collect_data_qubit(qubit_props):
     readout_length = qubit_props.get("readout_length", (None,))[0]
     return np.array([t1, t2, frequency, anharmonicity, readout_error, prob_meas0_prep1, prob_meas1_prep0, readout_length])
     
+def collect_data_gates(properties, qub):
+    id_props = properties.gate_property("id", qub)
+    id_error = id_props.get('gate_error', (None,))[0]
+    id_length = id_props.get('gate_length', (None,))[0]
+
+    rz_props = properties.gate_property("rz", qub)
+    rz_error = rz_props.get('gate_error', (None,))[0]
+    rz_length = rz_props.get('gate_length', (None,))[0]
+
+    sx_props = properties.gate_property("sx", qub)
+    sx_error = sx_props.get('gate_error', (None,))[0]
+    sx_length = sx_props.get('gate_length', (None,))[0]
+
+    rx_props = properties.gate_property("rx", qub)
+    rx_error = rx_props.get('gate_error', (None,))[0]
+    rx_length = rx_props.get('gate_length', (None,))[0]
+
+    measure_props = properties.gate_property("measure", qub)
+    measure_error = measure_props.get('gate_error', (None,))[0]
+    measure_length = measure_props.get('gate_length', (None,))[0]
+    
+    return np.array([
+        id_error, id_length,
+        rz_error, rz_length,
+        sx_error, sx_length,
+        rx_error, rx_length,
+        measure_error, measure_length
+    ])
+
 def collect_data_backend(backend):
-    data = []
+    data_qub = []
+    data_gates = []
     name = backend.name
     nb_qubits =  backend.num_qubits
     date = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     col_nom = np.full((nb_qubits, 2), [name, date])
 
     for index in range(nb_qubits):
-        data.append(
+        data_qub.append(
             collect_data_qubit(
                 backend.properties().qubit_property(index)
                 ))
-    data = np.c_[col_nom, data]
-
-    df = pd.DataFrame(
-        data=data, 
-        columns=["nom","time","T1", "T2", "frequency", "anharmonicity", "readout_error", "prob_meas0_prep1", "prob_meas1_prep0", "readout_length"]
+        data_gates.append(
+            collect_data_gates(
+                backend.properties(), index
+            )
         )
-    return df
+
+    # Merging the properties of gates and qubits to create a df
+    times_qub_data = np.c_[col_nom, data_qub]
+    data_add_gates = np.c_[times_qub_data, data_gates]
+    df_tot = pd.DataFrame(
+        data=data_add_gates, 
+        columns = ["nom","time",
+               "T1", "T2", "frequency", "anharmonicity","readout_error","prob_meas0_prep1", "prob_meas1_prep0", "readout_length", 
+                "id_error", "id_length", "rz_error", "rz_length", "sx_error", "sx_length", "rx_error", "rx_length", "measure_error", "measure_length"]
+                )
+    
+    # Only qubits data
+    df_qub = pd.DataFrame(
+        data=times_qub_data, 
+        columns = ["nom","time",
+               "T1", "T2", "frequency", "anharmonicity","readout_error","prob_meas0_prep1", "prob_meas1_prep0", "readout_length"
+                ]
+                )
+    
+    # Only gates data
+    times_gates_data = np.c_[col_nom, data_gates]
+    df_gates = pd.DataFrame(
+        data=times_gates_data, 
+        columns = ["nom","time",
+               "id_error", "id_length", "rz_error", "rz_length", "sx_error", "sx_length", "rx_error", "rx_length", "measure_error", "measure_length"
+                ]
+                )
+
+    return df_tot, df_qub, df_gates
 
 def full_collect(service):
     backends = service.backends(simulator=False, operational=True)
     
-    datas = []
+    datas_tot = []
+    datas_qub = []
+    datas_gates = []
     for back in backends:
-        datas.append(
-            collect_data_backend(back)
-        )
-    df = pd.concat(datas, ignore_index=True)
-    return df
+        df_tot, df_qub, df_gates = collect_data_backend(back)
+        datas_tot.append(df_tot)
+        datas_qub.append(df_qub)
+        datas_gates.append(df_gates)
+    complet_set = pd.concat(datas_tot, ignore_index=True)
+    qubit_set = pd.concat(datas_qub, ignore_index=True)
+    gates_set = pd.concat(datas_gates, ignore_index=True)
+    return complet_set, qubit_set, gates_set
 
-def create_csv(data):
-    folder = "/home/gwenn/Desktop/Projet/data"
+# 3 fonctions de créations de csv
+def create_csv_complet(data):
+    folder = "/home/gwenn/Desktop/Projet/data_json/"
     date = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-    name_file = folder+"/ibm_backends_"+ date + ".csv"
-    return data.to_csv(name_file, index=False)
+    name_file = folder+"ibm_backends_complet_"+ date + ".json"
+    return data.to_json(name_file, index=False)
+
+def create_csv_qubit(data):
+    folder = "/home/gwenn/Desktop/Projet/data_json/"
+    date = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    name_file = folder+"ibm_backends_qubit_"+ date + ".json"
+    return data.to_json(name_file, index=False)
+
+def create_csv_gates(data):
+    folder = "/home/gwenn/Desktop/Projet/data_json/"
+    date = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    name_file = folder+"ibm_backends_gates_"+ date + ".json"
+    return data.to_json(name_file, index=False)
 
 if __name__=="__main__":
     service = connect()
-    collected_data = full_collect(service)
-    create_csv(collected_data)
+    complete_data, qubit_data, gates_data = full_collect(service)
+    create_csv_complet(complete_data)
+    create_csv_qubit(qubit_data)
+    create_csv_gates(gates_data)
---
+++ merge_json.py
@@ -0,0 +1,25 @@
+import json
+import datetime
+import os
+
+import pandas as pd
+
+
+#insert : {complet, gates or qubit}
+def combineJson(prefixe_name):
+    folder = "/home/gwenn/Desktop/Projet/data_json/"
+    date = datetime.datetime.now().strftime("%Y-%m-%d_%H")
+    elements = os.listdir(folder)
+    files = []
+
+    for name_file in elements:
+        file = pd.read_json(folder+"/"+name_file)
+        files.append(file)
+    
+    merged_file = pd.concat(files, ignore_index=True)
+    output_folder = "/home/gwenn/Desktop/Projet/data_hour/"
+    name_file = output_folder+"ibm_backend_" + prefixe_name + "_hourly_total" + date +".json"
+    return merged_file.to_json(name_file, index=False)
+
+if __name__=="__main__":
+    combineJson()
