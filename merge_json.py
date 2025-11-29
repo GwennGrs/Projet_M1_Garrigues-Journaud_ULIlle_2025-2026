9abac2b84126ba5d5c68494e203c6226f625aca3
Gwenn <gwenn.garrigues@gmail.com>
2025-11-29 22:12:14 +0100

    feature : adding merging code run each hour

--- merge_json.py
+++ merge_json.py
@@ -13,13 +13,18 @@ def combineJson(prefixe_name):
     files = []
 
     for name_file in elements:
-        file = pd.read_json(folder+"/"+name_file)
-        files.append(file)
+        if prefixe_name in name_file and name_file.endswith(".json"):
+            print(name_file)
+            file = pd.read_json(folder+"/"+name_file)
+            files.append(file)
     
     merged_file = pd.concat(files, ignore_index=True)
     output_folder = "/home/gwenn/Desktop/Projet/data_hour/"
-    name_file = output_folder+"ibm_backend_" + prefixe_name + "_hourly_total" + date +".json"
+    name_file = output_folder+"ibm_backend_" + prefixe_name + "_hourly_total_" + date +".json"
     return merged_file.to_json(name_file, index=False)
 
 if __name__=="__main__":
-    combineJson()
+    combineJson("qubit")
+    combineJson("complet")
+    combineJson("gates")
+
